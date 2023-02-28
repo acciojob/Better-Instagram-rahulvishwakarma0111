f52d9fc7a4a1bae871e0c2c493db9e11aedea482
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-02-28 12:15:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+// Get all the draggable divs
+const divs = document.querySelectorAll('.image');
+
+// Set the position of each div to absolute
+divs.forEach((div) => {
+  div.style.position = 'absolute';
+});
+
+// Initialize the dragStartIndex variable
+let dragStartIndex;
+
+// Add a dragstart event listener to each div
+divs.forEach((div, index) => {
+  div.addEventListener('dragstart', () => {
+    dragStartIndex = index;
+  });
+});
+
+// Add a dragover event listener to each div
+divs.forEach((div) => {
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+});
+
+// Add a drop event listener to each div
+divs.forEach((div, index) => {
+  div.addEventListener('drop', () => {
+    // Swap the background images of the two divs
+    let temp = divs[dragStartIndex].style.backgroundImage;
+    divs[dragStartIndex].style.backgroundImage = div.style.backgroundImage;
+    div.style.backgroundImage = temp;
+  });
+});
